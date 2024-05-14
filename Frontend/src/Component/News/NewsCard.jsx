5318a4d09c1d406781cc55980bd0d98faeb0bea2
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-05-14 22:00:20 +0530

    Chnages In Vercel.json For hosting conf

--- Frontend/src/Component/News/NewsCard.jsx
+++ Frontend/src/Component/News/NewsCard.jsx
@@ -14,13 +14,13 @@ function NewsCard(newsobj) {
             <Container sx={{pr:2}}>
                 <hr />
                 <Grid container sx={{pt:2,pb:2}}>
-                    <Grid item xs={6} sx={{pl:2,pt:2,}}>
+                    <Grid item xs={6}  sx={{pl:2,pt:2,}}>
                     <a href={newsobj.news.link} style={{textDecoration:'none'}} target="blank"> 
                      <Typography variant="h5" sx={{fontWeight:"bold", color:'black',fontFamily:"Georgia",'&:hover':{color:"grey",cursor:'pointer'}}}>{newsobj.news.title}</Typography>
                      </a>
                         <Typography variant="subtitle1" sx={{fontFamily:"Garamond",fontSize:"20px",mt:1,lineHeight:"20px"}}>{newsobj.news.excerpt}</Typography>
                             </Grid>
-                    <Grid xs={1}></Grid>
+                    <Grid xs={1} ></Grid>
                     <Grid item xs={5} > 
 
                     <img style={{ height: "235px",width:"100%", borderRadius: "5px"}} src={newsobj.news.media} alt="" /> 
