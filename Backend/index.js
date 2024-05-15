23ba4cd4edfe74877defc7afcba92094df8bf6d5
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-05-15 23:30:54 +0530

    Resolve CORS Error

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Enable CORS for all routes with specific origin and methods
 app.use(cors({
-  origin: '', // Replace '*' with the specific origin of your frontend application
+  origin: 'https://info-vista-deou.vercel.app', // Replace '*' with the specific origin of your frontend application
   methods: ['POST', 'GET'],
   credentials: true,
 }));
