22f0daf50ba1632452d6339e9fd2f641e040e30b
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-05-23 20:04:47 +0530

    Added Col-12 for mobile size view of news

--- Frontend/src/Component/News/NewsCard.jsx
+++ Frontend/src/Component/News/NewsCard.jsx
@@ -14,14 +14,15 @@ function NewsCard(newsobj) {
             <Container sx={{pr:2}}>
                 <hr />
                 <Grid container sx={{pt:2,pb:2}}>
-                    <Grid item xs={6}  sx={{pl:2,pt:2,}}>
+                    <Grid item xs={12} lg={6} md={6} lg={6} xl={6}   sx={{pl:2,pt:2,}}>
                     <a href={newsobj.news.link} style={{textDecoration:'none'}} target="blank"> 
                      <Typography variant="h5" sx={{fontWeight:"bold", color:'black',fontFamily:"Georgia",'&:hover':{color:"grey",cursor:'pointer'}}}>{newsobj.news.title}</Typography>
                      </a>
                         <Typography variant="subtitle1" sx={{fontFamily:"Garamond",fontSize:"20px",mt:1,lineHeight:"20px"}}>{newsobj.news.excerpt}</Typography>
                             </Grid>
                     <Grid xs={1} ></Grid>
-                    <Grid item xs={5} > 
+
+                    <Grid item  xs={12} lg={5} md={5} lg={5} xl={5}   > 
 
                     <img style={{ height: "235px",width:"100%", borderRadius: "5px"}} src={newsobj.news.media} alt="" /> 
           <Box sx={{display:"flex"}}><a href={newsobj.news.link} style={{textDecoration:'none'}} target="blank"> <Typography sx={{color:"skyblue",mr:2}}>{newsobj.news.author}</Typography></a> <Typography sx={{color:'grey'}}>• Published {formattedDate}</Typography></Box>
